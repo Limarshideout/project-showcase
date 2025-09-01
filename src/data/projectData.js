ec56c0277e36de2bd80904ca9b2f3ed828333a71
Limarshideout <pennyortiz964@gmail.com>
2025-09-01 06:55:33 -0400

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -10,7 +10,7 @@ export const projects = [
       en: "",
       es: "",
     },
-    image: "",
+    image: "https://github.com/Limarshideout/pink-site.git",
     liveSite: "",
     githubRepo: "",
   },
